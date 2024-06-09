a9fa958c6e9faf6674e019c1fce6c766cbf7a07b
ridham <134303835+rhythm239@users.noreply.github.com>
2024-06-09 18:56:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const button = document.getElementById("enterBtn");
 const para = document.getElementById("status");
 
-    button.addEventListener("click", () => {para.innerHTML = "<h1>Entered the Metaverse</h1>"});
+    button.addEventListener("click", () => {para.innerHTML = "<h1>Entered Metaverse</h1>"});
